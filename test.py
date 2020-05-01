0625aedcf2a7b41e7e614916e8f703e6d2713c39
pikabol88 <nena.2000@mail.ru>
2020-05-01 12:38:52 -0700

    Добавлена конвертация частоты дискретизации

--- test.py
+++ test.py
@@ -3,10 +3,11 @@
 #from vosk import Model, KaldiRecognizer
 import sys
 #import os
-#import wave
+import wave
 from convert import conv_mp3_to_wav
 from recognize import recognize 
 from infile import save_in_file
+from downsample import reset_sample
 
 audio = sys.argv[1]
 audio = conv_mp3_to_wav(str(audio))
@@ -18,3 +19,10 @@ out = out.replace('wav','txt')
 out = "text/" + out
 
 save_in_file(result,out)
+
+wf = wave.open(path, "rb")
+sample = wf.getframerate()
+
+if sample!=8000 and sample!=16000 and sample!=32000 and sample!=48000:
+	#добавить поиск размера
+	reset_sample(path,48000)
